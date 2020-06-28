ef1c33904b1487592d522261a3e0facefdc7d4a2
Fryderyk Huang <fryderyk.huang@gmail.com>
2020-06-28 18:59:09 +0800

    update version number.

--- nuget_push.sh
+++ nuget_push.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+pushd src/MessagePack.Annotations
+dotnet build -c Release || exit 1
 pushd src/MessagePack
 dotnet build -c Release || exit 1
 popd
 
-#nuget push "`ls -Art bin/Packages/Release/NuGet/*.nupkg | grep 'Flexem.MessagePack.Annotations' | tail -n 1`" -Source https://nexus.flexem.net/repository/nuget-flexem/
+nuget push "`ls -Art bin/Packages/Release/NuGet/*.nupkg | grep 'MessagePack.Annotations.[0-9]' | tail -n 1`" -Source https://nexus.flexem.net/repository/nuget-flexem/
 nuget push "`ls -Art bin/Packages/Release/NuGet/*.nupkg | grep 'Flexem.MessagePack.[0-9]' | tail -n 1`" -Source https://nexus.flexem.net/repository/nuget-flexem/
